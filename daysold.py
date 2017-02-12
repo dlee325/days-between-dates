7c2a29b9c067b2f549ace2a4d8cb616bcf27f729
dlee325 <dlee325@gmail.com>
2017-02-12 19:19:40 +0100

    Final version of program that calculates the amount of days between two dates.

---
+++ daysold.py
@@ -0,0 +1,61 @@
+# Given your birthday and the current date, calculate your age 
+# in days. Compensate for leap days. Assume that the birthday 
+# and current date are correct dates (and no time travel). 
+# Simply put, if you were born 1 Jan 2012 and todays date is 
+# 2 Jan 2012 you are 1 day old.
+
+def isLeapYear(year):
+	if year % 400 == 0:
+		return True
+	if year % 100 == 0:
+		return False
+	if year % 4 == 0:
+		return True
+	return False
+
+
+def daysInMonth(year, month):
+	if month == 1 or month == 3 or month == 5 or month == 7 or month == 8 or month == 10 or month == 12:
+		return 31
+	else:
+		if month == 2:
+			if isLeapYear(year):
+				return 29
+			else:
+				return 28
+		else:
+			return 30
+
+def nextDay(year, month, day):
+    """Simple version: assume every month has 30 days"""
+    if day < daysInMonth(year,month):
+        return year, month, day + 1
+    else:
+        if month == 12:
+            return year + 1, 1, 1
+        else:
+            return year, month + 1, 1
+        
+def dateIsBefore(year1, month1, day1, year2, month2, day2):
+    """Returns True if year1-month1-day1 is before
+       year2-month2-day2. Otherwise, returns False."""
+    if year1 < year2:
+        return True
+    if year1 == year2:
+        if month1 < month2:
+            return True
+        if month1 == month2:
+            return day1 < day2
+    return False        
+
+def daysBetweenDates(year1, month1, day1, year2, month2, day2):
+    """Returns the number of days between year1/month1/day1
+       and year2/month2/day2. Assumes inputs are valid dates
+       in Gregorian calendar."""
+    # program defensively! Add an assertion if the input is not valid!
+    assert dateIsBefore(year1, month1, day1, year2, month2, day2)
+    days = 0
+    while dateIsBefore(year1, month1, day1, year2, month2, day2):
+        year1, month1, day1 = nextDay(year1, month1, day1)
+        days += 1
+    return days
